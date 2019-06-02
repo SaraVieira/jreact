23cb9270eb7f87ef247ec22778e59cbe37b555c0
Sara Vieira <hey@iamsaravieira.com>
2019-06-02 17:27:14 +0200

    initial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `JReact`,
+    description: `why not`,
+    author: `@freezydorito`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,126 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useEffect, useRef } from "react"
+import Reward from "react-rewards"
+import { keyframes } from "styled-components"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Animation from "../components/animation"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const spaceOut = keyframes`
+  from {
+    letter-spacing: 0;
+    font-size: 50px;
+  }
+
+  to {
+    letter-spacing: 4.3px;
+    font-size: 60px;
+  }
+`
+
+const enter = keyframes`
+  from {
+  opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+`
+
+const IndexPage = () => {
+  const reward = useRef(null)
+  useEffect(() => {
+    reward.current.rewardMe()
+  }, [])
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div
+        css={`
+          display: flex;
+          width: 100vw;
+          height: 100vh;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column;
+        `}
+      >
+        <Reward
+          config={{
+            angle: 90,
+            decay: 0.91,
+            spread: 80,
+            startVelocity: 35,
+            elementCount: 300,
+            elementSize: 8,
+            zIndex: 10,
+            springAnimation: true,
+          }}
+          ref={reward}
+          type="confetti"
+        >
+          <h1
+            css={`
+              font-weight: bold;
+              animation: ${spaceOut} 2s linear;
+              animation-fill-mode: forwards;
+            `}
+          >
+            JReact
+          </h1>
+        </Reward>
+        <Animation />
+        <code
+          css={`
+            font-family: monospace;
+            font-size: 24px;
+            animation: ${enter} 2s linear;
+            animation-fill-mode: forwards;
+            opacity: 0;
+            animation-delay: 3s;
+          `}
+        >
+          npm i jreact-lib
+        </code>
+        <ul
+          css={`
+            font-family: sans-serif;
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            margin-top: 20px;
+            text-transform: uppercase;
+            opacity: 0;
+            animation: ${enter} 2s linear;
+            animation-fill-mode: forwards;
+            animation-delay: 3s;
+          `}
+        >
+          <li>
+            <a
+              css={`
+                color: white;
+                margin-right: 10px;
+              `}
+              href="https://github.com/walaura/jquery-jsx-pragma"
+              target="_blank"
+            >
+              Github
+            </a>
+            <a
+              css={`
+                color: white;
+              `}
+              href="https://www.npmjs.com/package/jreact-lib"
+              target="blank"
+            >
+              NPM
+            </a>
+          </li>
+        </ul>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
